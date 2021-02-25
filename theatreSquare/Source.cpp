ef98bf3eb7edd0ceba875023bcbcfa061693d88d
himchawla <himchawla43@gmail.com>
2021-02-26 09:56:47 +1300

    frequency

--- theatreSquare/Source.cpp
+++ theatreSquare/Source.cpp
@@ -12,12 +12,15 @@ struct street
 	int inter2;
 	std::string name;
 	int timeOfCompletion;
+	int redLightTime;
+	int frequency;
 };
 
 struct inter
 {
 	int id;
-	std::vector<inter*> next;
+	std::vector<street*> next;
+	std::vector<street*> prev;
 };
 
 struct car
@@ -26,6 +29,11 @@ struct car
 	int timeOfCompletion;
 	std::vector<street*> path;
 };
+
+bool compare(const street& a, const street& b)
+{
+	return a.frequency < b.frequency;
+}
 
 
 void main()
@@ -63,9 +71,13 @@ void main()
 	{
 		tempStreet = new street;
 		fin >> tempStreet->inter1;
+		tempStreet->frequency = 0;
 		fin >> tempStreet->inter2;
 		fin >> tempStreet->name;
 		fin >> tempStreet->timeOfCompletion;
+
+		intersections[tempStreet->inter1]->next.push_back(tempStreet);
+		intersections[tempStreet->inter2]->prev.push_back(tempStreet);
 
 		streets.push_back(tempStreet);
 	}
@@ -83,6 +95,7 @@ void main()
 			{
 				if (t == j->name)
 				{
+					j->frequency++;
 					tempCar->path.push_back(j);
 					break;
 				}
@@ -97,6 +110,24 @@ void main()
 		cars.push_back(tempCar);
 	}
 
+
+	for (auto i : intersections)
+	{
+		std::sort(i->prev.begin(), i->prev.end(), compare);
+		int set = 1;
+		for (auto j:i->prev)
+		{
+			if (j->frequency == 0)
+				j->redLightTime = 0;
+			else
+				j->redLightTime = set++;
+
+		}
+	}
+
+
+
+	
 
 	
 
